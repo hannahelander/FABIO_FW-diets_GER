8b699894090a27f93a42690d7b10e4e63154ecf9
hannahelander <hanna.helander@transition.uni-freiburg.de>
2020-02-07 17:15:17 +0100

    update

--- 5c_visualization_diet_bars.R
+++ 5c_visualization_diet_bars.R
@@ -4,7 +4,7 @@ library(tidyr)
 Diets <- read.csv2(file ="input/Diets_3.0.csv" )
 
 y <- c('D. Status Quo ', 'C. German Rec. ',  'B. Sust. Diet ', 'A. Vegetarian ')
-data <- data.frame( "Cereals"          = c(sum(Diets[Diets$category == "Cereals ", 5]), sum(Diets[Diets$category == "Cereals ", 6]), 
+data_f <- data.frame( "Cereals"          = c(sum(Diets[Diets$category == "Cereals ", 5]), sum(Diets[Diets$category == "Cereals ", 6]), 
                                            sum(Diets[Diets$category == "Cereals ", 7]),  sum(Diets[Diets$category == "Cereals ", 8])),
                     "Potatoes"        = c(sum(Diets[Diets$category == "Potatoes & roots", 5]), sum(Diets[Diets$category == "Potatoes & roots", 6]), 
                                           sum(Diets[Diets$category == "Potatoes & roots", 7]),  sum(Diets[Diets$category == "Potatoes & roots", 8])),
@@ -29,10 +29,67 @@ data <- data.frame( "Cereals"          = c(sum(Diets[Diets$category == "Cereals 
                     row.names = c('Status Quo', 'National recommendations', 'EAT Lancet sustainable reference diet', 'EAT veg'))
 
 # covert milk (primary product equivalents) in Y_eaten to consumed weight
-Milk_exclButter <- 7297058.899 # from BMEL (Milcherzeugnis zusammen minus buttermilcherzeugnis)
+Milk_exclButter <- 7297058.899 # in Tonnes, from BMEL (Milcherzeugnis zusammen minus buttermilcherzeugnis)
 conv_rate <- sum(Y_SQ_eaten[index$DGE_group == "Milk"])/Milk_exclButter
+data_f$Milk <- data_f$Milk /conv_rate # do hopefully apply to all!
+
+#write.csv2(data_f, "output/diets_overview_cons.Weight.csv") # documentation of diets in actual weight
+
+data <- cbind(y, data_f)
+
+
+                  
+
+p <- plot_ly(data, x = ~Cereals, y = y, type = 'bar', orientation = 'h', name = 'Cereals',
+             marker = list(color = 'rgba(212, 162, 106, 0.6)',
+                           line = list(color = 'rgba(2212, 162, 106, 1.0)',
+                                       width = 0))) %>%
+  add_trace(x = ~Potatoes, name = 'Potatoes & roots',
+            marker = list(color = 'rgba(128, 77, 21, 0.6)',
+                          line = list(color = 'rgba(128, 77, 21, 1.0)',
+                                      width = 0))) %>%
+  add_trace(x = ~Vegetables, name = 'Vegetables',
+            marker = list(color = 'rgba(0, 61, 25, 0.6)',
+                          line = list(color = 'rgba(0, 61, 25, 1.0)',
+                                      width = 0))) %>%
+  add_trace(x = ~Fruits, name = 'Fruits',
+            marker = list(color = 'rgba(41, 122, 74, 0.6)',
+                          line = list(color = 'rgba(41, 122, 74, 1.0)',
+                                      width = 0))) %>%
+  add_trace(x = ~Pulses_beans_nuts, name = 'Pulses, beans & nuts',
+            marker = list(color = 'rgba(2, 38, 54, 0.6)',
+                          line = list(color = 'rgba(2, 38, 54, 1.0)',
+                                      width = 0))) %>%
+  add_trace(x = ~Vegetable_oils, name = 'Vegetable oils',
+            marker = list(color = 'rgba(39, 87, 107, 0.6)',
+                          line = list(color = 'rgba(112, 80, 80, 1.0)',
+                                      width = 0))) %>%
+  add_trace(x = ~Milk, name = 'Milk & products',
+            marker = list(color = 'rgba(212, 127, 106, 0.6)',
+                          line = list(color = 'rgba(212, 127, 106, 1.0)',
+                                      width = 0))) %>%
+  add_trace(x = ~Eggs, name = 'Eggs',
+            marker = list(color = 'rgba(170, 78, 57, 0.6)',
+                          line = list(color = 'rgba(170, 78, 57, 1.0)',
+                                      width = 0))) %>%
+  add_trace(x = ~Fish, name = 'Fish',
+            marker = list(color = 'rgba(128, 42, 21, 0.6)',
+                          line = list(color = 'rgba(128, 42, 21, 1.0)',
+                                      width = 0))) %>%
+  add_trace(x = ~Meat, name = 'Meat',
+            marker = list(color = 'rgba(85, 16, 0, 0.6)',
+                          line = list(color = 'rgba(85, 16, 0,  1.0)',
+                                      width = 0))) %>%
+  add_trace(x = ~Sugar_Alcohol, name = 'Sugar & Alcohol',
+            marker = list(color = 'rgba(107, 117, 159, 0.6)',
+                          line = list(color = 'rgba(107, 117, 159, 1.0)',
+                                      width = 0))) %>%
   
-data$Milk <- data$Milk /conv_rate # do hopefully apply to all!
+  layout(barmode = 'stack',
+         xaxis = list(title = "", size = 30),
+         yaxis = list(title = ""))
+
+p
 
 
 
@@ -88,60 +145,6 @@ data$Milk <- data$Milk /conv_rate # do hopefully apply to all!
 #                     row.names = c('Status Quo', 'National recommendations', 'EAT Lancet sustainable reference diet', 'Plant based reference diet', 'EAT veg'))
 ########################
 
-data <- cbind(y, data)
-
-                  
-
-p <- plot_ly(data, x = ~Cereals, y = y, type = 'bar', orientation = 'h', name = 'Cereals',
-             marker = list(color = 'rgba(212, 162, 106, 0.6)',
-                           line = list(color = 'rgba(2212, 162, 106, 1.0)',
-                                       width = 0))) %>%
-  add_trace(x = ~Potatoes, name = 'Potatoes & roots',
-            marker = list(color = 'rgba(128, 77, 21, 0.6)',
-                          line = list(color = 'rgba(128, 77, 21, 1.0)',
-                                      width = 0))) %>%
-  add_trace(x = ~Vegetables, name = 'Vegetables',
-            marker = list(color = 'rgba(0, 61, 25, 0.6)',
-                          line = list(color = 'rgba(0, 61, 25, 1.0)',
-                                      width = 0))) %>%
-  add_trace(x = ~Fruits, name = 'Fruits',
-            marker = list(color = 'rgba(41, 122, 74, 0.6)',
-                          line = list(color = 'rgba(41, 122, 74, 1.0)',
-                                      width = 0))) %>%
-  add_trace(x = ~Pulses_beans_nuts, name = 'Pulses, beans & nuts',
-            marker = list(color = 'rgba(2, 38, 54, 0.6)',
-                          line = list(color = 'rgba(2, 38, 54, 1.0)',
-                                      width = 0))) %>%
-  add_trace(x = ~Vegetable_oils, name = 'Vegetable oils',
-            marker = list(color = 'rgba(39, 87, 107, 0.6)',
-                          line = list(color = 'rgba(112, 80, 80, 1.0)',
-                                      width = 0))) %>%
-  add_trace(x = ~Milk, name = 'Milk & products',
-            marker = list(color = 'rgba(212, 127, 106, 0.6)',
-                          line = list(color = 'rgba(212, 127, 106, 1.0)',
-                                      width = 0))) %>%
-  add_trace(x = ~Eggs, name = 'Eggs',
-            marker = list(color = 'rgba(170, 78, 57, 0.6)',
-                          line = list(color = 'rgba(170, 78, 57, 1.0)',
-                                      width = 0))) %>%
-  add_trace(x = ~Fish, name = 'Fish',
-            marker = list(color = 'rgba(128, 42, 21, 0.6)',
-                          line = list(color = 'rgba(128, 42, 21, 1.0)',
-                                      width = 0))) %>%
-  add_trace(x = ~Meat, name = 'Meat',
-            marker = list(color = 'rgba(85, 16, 0, 0.6)',
-                          line = list(color = 'rgba(85, 16, 0,  1.0)',
-                                      width = 0))) %>%
-  add_trace(x = ~Sugar_Alcohol, name = 'Sugar & Alcohol',
-            marker = list(color = 'rgba(107, 117, 159, 0.6)',
-                          line = list(color = 'rgba(107, 117, 159, 1.0)',
-                                      width = 0))) %>%
-  
-  layout(barmode = 'stack',
-         xaxis = list(title = "", size = 30),
-         yaxis = list(title = ""))
-
-p
 
 #EXAMPLE:
 library(plotly)

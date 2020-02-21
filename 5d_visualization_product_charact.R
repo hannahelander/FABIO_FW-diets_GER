0d3c655198ac242c4abf6bb9303aa036e95d5dc6
hannahelander <hanna.helander@transition.uni-freiburg.de>
2020-02-21 15:04:17 +0100

    graphs with proper labels

--- 5d_visualization_product_charact.R
+++ 5d_visualization_product_charact.R
@@ -1,12 +1,59 @@
-data1 = iris
-
 library(plotly)
+library(tidyr)
 
 prod_cat <- read.csv2("input/product_characteristics.csv")
+prod_cat <- prod_cat[-9,]
 
-p <- plot_ly(data = prod_cat, x = ~FW, y = ~Biomass, color= ~X, colors = "Set1", size = 3)
-p <- plot_ly(data = prod_cat, x = ~FW, y = ~Land, color= ~X, colors = "Set1", size = 3)
-p <- plot_ly(data = prod_cat, x = ~FW, y = ~Water, color= ~X, colors = "Set3", size = 3)
+
+#colors = list(cereals = 'rgba(212, 162, 106, 0.6)', ) 
+
+f <- list(
+  family = "Arial",
+  size = 12,
+  color = "black")
+
+#### Biomass
+
+x <- list(
+  title = "Food waste shares",
+  titlefont = f)
+y <- list(
+  title = "Biomass footprint (kg / 1000 kcal)",
+  titlefont = f)
+
+p <- plot_ly(data = prod_cat, x = ~FW, y = ~Biomass, color= ~X, colors = "Paired", size = 3) %>%
+  layout(xaxis = x, yaxis = y)
+
+p
+
+#### Cropland
+
+x <- list(
+  title = "Food waste shares",
+  titlefont = f)
+y <- list(
+  title = "Cropland footprint (hectars / 1000 kcal)",
+  titlefont = f)
+
+p <- plot_ly(data = prod_cat, x = ~FW, y = ~Land, color= ~X, colors = "Paired", size = 3) %>%
+  layout(xaxis = x, yaxis = y)
+
+p
+
+
+#### Blue water
+
+x <- list(
+  title = "Food waste shares",
+  titlefont = f)
+y <- list(
+  title = "Blue water footprint (m3 / 1000 kcal)",
+  titlefont = f)
+
+p <- plot_ly(data = prod_cat, x = ~FW, y = ~Water, color= ~X, colors = "Paired", size = 3) %>%
+  layout(xaxis = x, yaxis = y)
+
+
 
 p
 

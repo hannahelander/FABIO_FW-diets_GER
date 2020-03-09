7d1757a1e6d96a0511b9c0954cd178eb3110f3a2
hannahelander <hanna.helander@transition.uni-freiburg.de>
2020-03-09 19:51:51 +0100

    colors ok!

--- 5d_visualization_product_charact.R
+++ 5d_visualization_product_charact.R
@@ -1,8 +1,6 @@
 library(plotly)
 library(tidyr)
-#library(scales)
-#install.packages("ggplot2")
-#library(ggplot2)
+
 
 prod_cat <- read.csv2("input/product_characteristics.csv")
 prod_cat <- prod_cat[-9,]
@@ -13,7 +11,6 @@ color_values = c(rgb(212, 162, 106, maxColorValue = 255), rgb(128, 77, 21,   max
                       rgb(2, 38, 54,  maxColorValue = 255), rgb(39, 87, 107,   maxColorValue = 255),
                       rgb(212, 127, 106,  maxColorValue = 255), rgb(170, 78, 57,  maxColorValue = 255),
                       rgb(85, 16, 0,  maxColorValue = 255),rgb(107, 117, 159,  maxColorValue = 255))
-
 
 
 
@@ -37,20 +34,9 @@ y <- list(
   title = "Biomass footprint (kg / 1000 kcal)",
   titlefont = f)
 
-# fig <- plot_ly(data = prod_cat, type = 'scatter', mode = 'markers') 
-# fig <- fig %>%
-#   add_trace(x = ~FW,    y = ~Biomass,
-#             marker = list(color = color_values,
-#                           size = 18,
-#                           line = list(color = color_values, width = 2) )
-#   ) %>%
-#   layout(xaxis = x, yaxis = y) 
-# 
-# fig
-#
 
 p <- plot_ly(data = prod_cat, type='scatter', mode = 'markers', x = ~FW, y = ~Biomass, color= ~X,
-            colors = color_values,  size = 3) %>%
+            colors = color_values,  size = I(150))  %>%
  layout(xaxis = x, yaxis = y)
 
 p
@@ -66,22 +52,9 @@ y <- list(
   title = "Cropland footprint (hectars / 1000 kcal)",
   titlefont = f)
 
-# fig <- plot_ly(data = prod_cat, type = 'scatter', mode = 'markers') 
-# fig <- fig %>%
-#   add_trace(x = ~FW,    y = ~Land,
-#             marker = list(color = color_values,
-#                           size = 18,
-#                           line = list(color = color_values, width = 2))
-#   ) %>%
-#   layout(xaxis = x, yaxis = y) 
-# 
-# fig
-
-
-######## version with labels
 
 p <- plot_ly(data = prod_cat,  type='scatter', mode = 'markers', x = ~FW, y = ~Land, color= ~X, 
-             colors =  color_values, size = 100) %>% 
+             colors =  color_values, size = I(150)) %>% 
   
   layout(xaxis = x, yaxis = y)
 
@@ -97,19 +70,9 @@ y <- list(
   title = "Blue water footprint (m3 / 1000 kcal)",
   titlefont = f)
 
-# fig <- plot_ly(data = prod_cat, type = 'scatter', mode = 'markers') 
-# fig <- fig %>%
-#   add_trace(x = ~FW,    y = ~Water,
-#             marker = list(color = color_values,
-#                           size = 18,
-#                           line = list(color = color_values, width = 2))
-#   ) %>%
-#   layout(xaxis = x, yaxis = y) 
-# 
-# fig
 
 p <- plot_ly(data = prod_cat, type='scatter', mode = 'markers', x = ~FW, y = ~Water, color= ~X, 
-             colors = color_values, size = 3) %>%
+             colors = color_values, size = I(150)) %>%
   layout(xaxis = x, yaxis = y)
 
 p
@@ -118,16 +81,7 @@ p
 
 #Plot
 
-# color_values_hex = c("#d4a269",rgb(128, 77, 21,   maxColorValue = 255), # need to adapt order!
-#                      rgb(0, 61, 25, 0 , maxColorValue = 255),rgb(41, 122, 74,   maxColorValue = 255),
-#                      rgb(2, 38, 54,  maxColorValue = 255), rgb(39, 87, 107,   maxColorValue = 255),
-#                      rgb(212, 127, 106,  maxColorValue = 255), rgb(170, 78, 57,  maxColorValue = 255),
-#                      rgb(85, 16, 0,  maxColorValue = 255),rgb(107, 117, 159,  maxColorValue = 255))
-# 
-# color_values = c('rgba(212, 162, 106, 0.6)','rgb(128, 77, 21, 0.6)', 
-#                  'rgb(0, 61, 25, 0 , 0.6)', 'rgb(41, 122, 74,  0.6)' , 'rgb(2, 38, 54, 0.6)', 
-#                  'rgb(39, 87, 107,  0.6)', 'rgb(212, 127, 106,  0.6)', 'rgb(170, 78, 57,  0.6)',
-#                  'rgb(85, 16, 0,  0.6)', 'rgb(107, 117, 159, 0.6)')
+
 
 #### ggplot(prod_cat, aes(x = FW, y = Biomass)) + 
 #   geom_point(aes(colour = X, size=Land)) + ylab("Impacts") + xlab("Food waste")+
